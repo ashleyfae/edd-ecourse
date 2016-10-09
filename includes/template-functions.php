d5f56080291bb565fc39565f377be37fefb5ec61
Ashley Evans <cazzyae@gmail.com>
2016-10-09 17:13:07 +0100

    Updated tempalte todo list.

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -4,11 +4,7 @@
  *
  * For modifying the front-end of the site.
  *
- * @todo      Plan for these pages:
- *       Dashboard - widgetized
- *       Course list (including ones not purchased)
- *       Single course page with list of modules/lessons
- *       Single lesson view
+ * @todo      Widgetized dashboard page.
  *
  * @package   edd-ecourse
  * @copyright Copyright (c) 2016, Ashley Gibson
