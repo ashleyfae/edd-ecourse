00eee170f8643bd9d61c6ff2211cdd75d3611d66
Ashley Evans <cazzyae@gmail.com>
2016-10-09 16:22:11 +0100

    Added index template.

---
+++ templates/ecourse-index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * E-Course Main Template
+ *
+ * This is the main template that gets loaded and checks to see which
+ * piece of content should actually be loaded. We have this main template
+ * mainly so we can perform permission checks.
+ *
+ * @package   edd-ecourse
+ * @copyright Copyright (c) 2016, Ashley Gibson
+ * @license   GPL2+
+ */
