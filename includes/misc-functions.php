cacac10cba881e1de1e71a81f4c85fedaa5e3ffa
Ashley Evans <cazzyae@gmail.com>
2016-11-07 14:10:31 +0000

    Register widget area.

--- includes/misc-functions.php
+++ includes/misc-functions.php
@@ -31,3 +31,26 @@ function edd_ecourse_get_dashboard_url() {
 
 	return apply_filters( 'edd_ecourse_dashboard_url', $url, $dashboard );
 }
+
+/**
+ * Register Widget Areas
+ *
+ * @since 1.0.0
+ * @return void
+ */
+function edd_ecourse_register_widget_areas() {
+
+	// Dashboard
+	register_sidebar( array(
+		'name'          => esc_html__( 'E-Course - Dashboard', 'edd-ecourse' ),
+		'id'            => 'ecourse-dashboard',
+		'description'   => esc_html__( 'Widgets to appear on the EDD E-Course dashboard page.', 'edd-ecourse' ),
+		'before_widget' => '<section id="%1$s" class="box %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>'
+	) );
+
+}
+
+add_action( 'widgets_init', 'edd_ecourse_register_widget_areas', 100 );
